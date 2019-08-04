a50316f48dd4dcfc2da1d0132f8384bc615ad558
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-04 16:21:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  for i in array do
+    new_array.push(yield(i))
+  end
+  return new_array
+end
+
+def reduce(array, sum=0)
+  value = 0;
+  for i in array do
+    value += yield
+  end
+  return value
+  
